a52e04040a3393f75a2676e89e43174af46e460d
pankajnimgade <pankaj.nimgade@gmail.com>
2016-01-19 20:40:47 +0530

    Initial commit for JavaCoding_tutorials :) I already like it.

---
+++ src/com/pk/nimgade/Main.java
@@ -0,0 +1,13 @@
+package com.pk.nimgade;
+
+/**
+ * This is the test class to check if the configuration for the
+ * project os wrking
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Print something");
+    }
+}
